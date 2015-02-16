5357b08c4dcfd99d90c7472cfb59205236ee36c3
Aaron Schumacher <ajschumacher@gmail.com>
2015-02-16 17:59:56 -0500

    two spaces to four spaces for indentation

--- code/parse_to_csv.py
+++ code/parse_to_csv.py
@@ -10,18 +10,18 @@ fields = ['ATTN_DATE_YMD', 'DBN', 'ATTN_PCT',
 rows = [fields]
 
 for filename in sorted(glob.glob('../xml/*.xml')):
-  with open(filename) as file:
-    # All data is on one long line.
-    line = file.readline()
-    # Remove ASCII decimal 26, "synchronous "
-    line = line.replace(chr(26), '')
-    root = etree.fromstring(line)
-    for item in root:
-      contents = dict()
-      for element in item:
-        if element.text: contents[element.tag] = element.text
-      rows.append([contents.get(field,'').encode('utf-8') for field in fields])
+    with open(filename) as file:
+        # All data is on one long line.
+        line = file.readline()
+        # Remove ASCII decimal 26, "synchronous "
+        line = line.replace(chr(26), '')
+        root = etree.fromstring(line)
+        for item in root:
+            contents = dict()
+            for element in item:
+                if element.text: contents[element.tag] = element.text
+            rows.append([contents.get(field,'').encode('utf-8') for field in fields])
 
 writer = csv.writer(sys.stdout)
 for line in rows:
-  writer.writerow(line)
+    writer.writerow(line)

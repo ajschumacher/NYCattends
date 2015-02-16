a664047085ccaa6997489415ebb4c8f2aefae567
Aaron Schumacher <ajschumacher@gmail.com>
2015-02-16 18:34:11 -0500

    more intelligent xml value-grabbing

--- code/parse_to_csv.py
+++ code/parse_to_csv.py
@@ -1,27 +1,23 @@
 #!/usr/bin/env python
 
 import glob
-from xml.etree import ElementTree as etree
+from xml.etree import ElementTree as ET
 import sys
 import csv
 
 fields = ['ATTN_DATE_YMD', 'DBN', 'ATTN_PCT',
           'SCHOOL_NAME', 'Borough', 'DistrictCode', 'LOC_CODE']
-rows = [fields]
-
-for filename in sorted(glob.glob('../xml/*.xml')):
-    with open(filename) as file:
-        # All data is on one long line.
-        line = file.readline()
-        # Remove ASCII decimal 26, "synchronous "
-        line = line.replace(chr(26), '')
-        root = etree.fromstring(line)
-        for item in root:
-            contents = dict()
-            for element in item:
-                if element.text: contents[element.tag] = element.text
-            rows.append([contents.get(field,'').encode('utf-8') for field in fields])
 
 writer = csv.writer(sys.stdout)
-for line in rows:
-    writer.writerow(line)
+writer.writerow(fields)
+
+for filename in sorted(glob.glob('../xml/*.xml')):
+    with open(filename) as f:
+        # All data is on one long line.
+        line = f.readline()
+    # Remove ASCII decimal 26, "synchronous "
+    line = line.replace(chr(26), '')
+    root = ET.fromstring(line)
+    for item in root:
+        row = [item.find(field).text or '' for field in fields]
+        writer.writerow([val.encode('utf-8') for val in row])
